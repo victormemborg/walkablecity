21753d9de9ce1efb5a07f43c88d2e81de4278231
Philip Rosenhagen <phro@itu.dk>
2026-02-14 19:00:31 +0100

    categorization

--- preprocessing/fullscale.py
+++ preprocessing/fullscale.py
@@ -4,45 +4,9 @@ from osmium import filter, osm, geom
 from shapely import wkt, Point
 import geopandas as gpd
 import pandas as pd
+from collections import defaultdict
 
-#################### Collect all amenities ####################
-
-data= "../../denmark-260208.osm.pbf"
-fp = osmium.FileProcessor(data).with_areas() \
-    .with_filter(filter.EntityFilter(osm.NODE | osm.AREA))\
-    .with_filter(filter.KeyFilter("amenity")
-)
-
-fab = geom.WKTFactory()
-
-def to_points(fp):
-    points: list[Point] = []
-    amenities: list[str] = []
-
-    for o in fp:
-        if o.is_node():
-            shape = wkt.loads(fab.create_point(o))
-        elif o.is_area():
-            shape = wkt.loads(fab.create_multipolygon(o))
-        else:
-            raise AssertionError(f"Unreachable: {o}")
-
-        points.append(shape.centroid)
-        amenities.append(o.tags.get("amenity"))
-        
-    return points, amenities
-
-points, amenities = to_points(fp)
-
-#################### Calculate ####################
-
-network = pdna.Network.from_hdf5("denmark.backup")
-
-WALKING_SPEED_KMPH = 4
-MAX_WALKING_TIME_MIN = 15
-max_distance = WALKING_SPEED_KMPH * 1000 / 60 * MAX_WALKING_TIME_MIN  # meters
-
-category_map = {
+CATEGORY_MAP = {
     # Education
     "school": "education",
     "university": "education",
@@ -60,29 +24,62 @@ category_map = {
     "museum": "culture",
 }
 
+#################### Collect all amenities ####################
+
+data= "../../denmark-260208.osm.pbf"
+fp = osmium.FileProcessor(data).with_areas() \
+    .with_filter(filter.EntityFilter(osm.NODE | osm.AREA))\
+    .with_filter(filter.KeyFilter("amenity")
+)
+
+def categorize(fp):
+    fab = geom.WKTFactory()
+    categorized_points: defaultdict[str, list[Point]] = defaultdict(list)
+
+    for o in fp:
+        amenity = o.tags.get("amenity")
+        category = CATEGORY_MAP.get(amenity)
+        if category is None:
+            continue
+
+        if o.is_node():
+            shape = wkt.loads(fab.create_point(o))
+        elif o.is_area():
+            shape = wkt.loads(fab.create_multipolygon(o))
+        else:
+            raise AssertionError(f"Unreachable: {o}")
+
+        categorized_points[category].append(shape.centroid)
+        
+    return categorized_points
+
+categorized_points = categorize(fp)
+
+#################### Calculate ####################
+
+network = pdna.Network.from_hdf5("denmark.backup")
+
+WALKING_SPEED_KMPH = 4
+MAX_WALKING_TIME_MIN = 15
+max_dist = WALKING_SPEED_KMPH * 1000 / 60 * MAX_WALKING_TIME_MIN  # meters
+
 print("loop begin")
 poi_distances = {}
-categorised_points
 
-for point, amenity in zip(points, amenities):
-    if amenity not in category_map.keys():
-        continue
-
+for category, points in categorized_points.items():
     network.set_pois(
-        amenity,
-        max_distance,
-        1,
-        point.x,
-        point.y,
+        category=category,
+        maxdist=max_dist,
+        maxitems=1,
+        x_col=[p.x for p in points],
+        y_col=[p.y for p in points],
     )
-
-    network.nodes_in_range
     
     d = network.nearest_pois(
-        max_distance,
-        amenity,
+        distance=max_dist,
+        category=category,
         num_pois=1,
-        max_distance=max_distance + 1,
+        max_distance=max_dist + 1,
     ).iloc[:, 0]
     
     poi_distances[amenity] = d
