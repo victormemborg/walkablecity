d733d15161bf22a9efa346f906cb4ff9da821324
Philip Rosenhagen <philiprosenhagen@gmail.com>
2026-03-23 18:43:35 +0100

    default style

--- 15-minute-cities/components/AccessibilityLayer.tsx
+++ 15-minute-cities/components/AccessibilityLayer.tsx
@@ -3,27 +3,28 @@ import { useMapEvent } from "react-leaflet";
 import VectorTileLayer from "./VectorTileLayer";
 import { AssertionError } from "assert";
 
-function zoomToTable(zoom: number): string {
+function zoomToLayer(zoom: number): string {
     if (zoom <= 8) return "scored_grids_pmtiles_p5";
-    if (zoom <= 11) return "scored_grids_pmtiles_p6";
+    if (zoom <= 12) return "scored_grids_pmtiles_p6";
     if (zoom <= 14) return "scored_grids_pmtiles_p7";
     if (zoom <= 18) return "scored_edges_pmtiles_p0";
     throw new AssertionError({message: "unhandled zoom level"});
 };
 
 export default function AccessibilityLayer({baseTileUrl, initialZoom}: {baseTileUrl: string, initialZoom: number}) {
-    const [table, setTable] = useState(() => zoomToTable(initialZoom));
+    const [layer, setLayer] = useState(() => zoomToLayer(initialZoom));
 
     useMapEvent("zoomend", e => {
-        const newTable = zoomToTable(e.target.getZoom());
-        setTable(newTable);
+        const newLayer = zoomToLayer(e.target.getZoom());
+        console.log(e.target.getZoom())
+        setLayer(newLayer);
     });
 
-    const url = `${baseTileUrl}${table}/{z}/{x}/{y}`;
+    const url = `${baseTileUrl}${layer}/{z}/{x}/{y}`;
     return (
         <VectorTileLayer
           url={url}
-          layerName={`${table}.pmtiles`}
+          layerName={layer}
         />
     )
 }
