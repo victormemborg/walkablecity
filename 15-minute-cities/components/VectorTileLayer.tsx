43f24050d51e513565e982337035b351d13ce6a9
Philip Rosenhagen <philiprosenhagen@gmail.com>
2026-03-25 20:04:38 +0100

    debugging visual artifacts

--- 15-minute-cities/components/VectorTileLayer.tsx
+++ 15-minute-cities/components/VectorTileLayer.tsx
@@ -24,7 +24,7 @@ async function fetchScoreRange(bounds: L.LatLngBounds, table: string): Promise<R
 
 const vectorTileLayerStyles = (colorFn: (score: number) => string) => new Proxy(
     {
-        // Specific styles:
+        // Specific layer styles:
         scored_edges_pmtiles_p0: (properties: Record<string, string>) => {
             const color = colorFn(Number(properties.score))
             return {
@@ -54,13 +54,16 @@ export default function VectorTileLayer({ url, layerName }: { url: string; layer
     const scoreRangeRef = useRef<Range>([0, BASE_MAX_SCORE]);
     const vectorGridRef = useRef<L.VectorGrid.Protobuf | null>(null);
 
-    // Taken from: https://gist.github.com/mlocati/7210513
+    // Modified from: https://gist.github.com/mlocati/7210513
     const colorGradient = (score: number) => {
-        const [min, max] = scoreRangeRef.current;
+        let [min, max] = scoreRangeRef.current;
+        // TODO: There is some discrepancy between the max reported by postgis, and the actual max.
+        // If actual max is higher than (this) reported max we get weird visual artifacts (black spots)
+        max = max + 500 
 
         const actual = score / BASE_MAX_SCORE * 100;
         const normalized = (score - min) / (max - min) * 100;
-        const effective = (actual + normalized*2) / 3
+        const effective = (actual*1.5 + normalized) / 2.5
 
         let r, g;
         const b = 0;
@@ -93,10 +96,21 @@ export default function VectorTileLayer({ url, layerName }: { url: string; layer
 
     // Mount/unmount the layer once
     useEffect(() => {
+        (L.DomEvent as any).fakeStop = (L.DomEvent as any).fakeStop ?? (() => {});
         const vectorGrid = L.vectorGrid.protobuf(url, {
             rendererFactory: L.canvas.tile,
-            interactive: false,
+            interactive: true,
             vectorTileLayerStyles: vectorTileLayerStyles(colorGradient),
+        });
+        
+        // For debugging
+        vectorGrid.on("click", e => {
+            console.log(e)
+            const props = e.layer.properties;
+            const latlng = e.latlng;
+
+            const content = `<strong>Score:</strong> ${props.score || "N/A"}` 
+            L.popup().setLatLng(latlng).setContent(content).openOn(map);
         });
 
         vectorGrid.addTo(map);
