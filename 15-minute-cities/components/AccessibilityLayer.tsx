49d019417740661e41de51b6bf69855b1da412de
Philip Rosenhagen <philiprosenhagen@gmail.com>
2026-03-28 03:11:04 +0100

    ignorant approach to check performance benefits

--- 15-minute-cities/components/AccessibilityLayer.tsx
+++ 15-minute-cities/components/AccessibilityLayer.tsx
@@ -1,26 +1,6 @@
 import { VectorSourceSpecification, FillLayerSpecification } from "maplibre-gl";
 import { Source, Layer } from "react-map-gl/maplibre";
 
-const gridsP6Source: VectorSourceSpecification = {
-	type: "vector" as const,
-	tiles: ["http://localhost:3001/scored_grids_pmtiles_p6/{z}/{x}/{y}.pbf"],
-	minzoom: 0,
-	maxzoom: 9
-};
-
-const gridsP6Style: FillLayerSpecification = {
-	id: "grids-p6-style",
-	type: "fill",
-	source: "grids-p6-source",
-	"source-layer": "scored_grids_pmtiles_p6", 
-	paint: {
-		"fill-color": "#9b0d0d",
-		"fill-opacity": 0.5
-	},
-	minzoom: 0,
-	maxzoom: 10
-};
-
 export default function AccessibilityLayer({tileUrl, layerId}: {tileUrl: string, layerId: string}) {
     const sourceId = `test-source`;
     const styleId = `test-style`;
--- 15-minute-cities/components/AccessibilityMap.tsx
+++ 15-minute-cities/components/AccessibilityMap.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-import { useState } from "react";
 import Map, { Layer, Source, ViewStateChangeEvent} from "react-map-gl/maplibre";
-import type { FillLayerSpecification, VectorSourceSpecification } from 'maplibre-gl';
+import type { FillLayerSpecification, LineLayerSpecification, VectorSourceSpecification } from 'maplibre-gl';
 import "maplibre-gl/dist/maplibre-gl.css";
-import { AssertionError } from "assert";
-import AccessibilityLayer from "./AccessibilityLayer";
+
+const gridsP5Source: VectorSourceSpecification = {
+	type: "vector" as const,
+	tiles: ["http://localhost:3001/scored_grids_pmtiles_p5/{z}/{x}/{y}.pbf"],
+	minzoom: 0,
+	maxzoom: 8
+};
+
+const gridsP5Style: FillLayerSpecification = {
+	id: "grids-p5-style",
+	type: "fill",
+	source: "grids-p5-source",
+	"source-layer": "scored_grids_pmtiles_p5", 
+	paint: {
+		"fill-color": "#9b0d0d",
+		"fill-opacity": 0.5
+	},
+	minzoom: 0,
+	maxzoom: 8
+};
 
 const gridsP6Source: VectorSourceSpecification = {
 	type: "vector" as const,
 	tiles: ["http://localhost:3001/scored_grids_pmtiles_p6/{z}/{x}/{y}.pbf"],
-	minzoom: 0,
-	maxzoom: 9
+	minzoom: 8,
+	maxzoom: 12
 };
 
 const gridsP6Style: FillLayerSpecification = {
@@ -23,15 +40,15 @@ const gridsP6Style: FillLayerSpecification = {
 		"fill-color": "#9b0d0d",
 		"fill-opacity": 0.5
 	},
-	minzoom: 0,
-	maxzoom: 10
+	minzoom: 8,
+	maxzoom: 12
 };
 
 const gridsP7Source: VectorSourceSpecification = {
 	type: "vector" as const,
 	tiles: ["http://localhost:3001/scored_grids_pmtiles_p7/{z}/{x}/{y}.pbf"],
-	minzoom: 10,
-	maxzoom: 18
+	minzoom: 12,
+	maxzoom: 14
 };
 
 const gridsP7Style: FillLayerSpecification = {
@@ -43,27 +60,36 @@ const gridsP7Style: FillLayerSpecification = {
 		"fill-color": "#2c0d9b",
 		"fill-opacity": 0.5
 	},
-	minzoom: 10,
+	minzoom: 12,
+	maxzoom: 14
+};
+
+const edgesP0Source: VectorSourceSpecification = {
+	type: "vector" as const,
+	tiles: ["http://localhost:3001/scored_edges_pmtiles_p0/{z}/{x}/{y}.pbf"],
+	minzoom: 14,
 	maxzoom: 18
 };
 
-function zoomToLayerId(zoom: number): string {
-    if (zoom <= 8) return "scored_grids_pmtiles_p5";
-    if (zoom <= 12) return "scored_grids_pmtiles_p6";
-    if (zoom <= 14) return "scored_grids_pmtiles_p7";
-    if (zoom <= 18) return "scored_edges_pmtiles_p0";
-    throw new AssertionError({message: "unhandled zoom level"});
+const edgesP0Style: LineLayerSpecification = {
+	id: "edges-p0-style",
+	type: "line",
+	source: "edges-p0-source",
+	"source-layer": "scored_edges_pmtiles_p0", 
+	paint: {
+		"line-color": "#2c0d9b",
+		"line-width": 2
+	},
+	minzoom: 14,
+	maxzoom: 19 // max + 1
 };
 
 export default function AccessibilityMap() {
 	const initialZoom = 11;
-	const [layerId, setLayerId] = useState(zoomToLayerId(initialZoom));
-	const tileUrl = `http://localhost:3001/${layerId}/{z}/{x}/{y}.pbf`;
 
 	const onZoomEnd = (e: ViewStateChangeEvent) => {
 		const zoom = e.target.getZoom();
 		console.log(`Zoom: ${zoom}`);
-		setLayerId(zoomToLayerId(zoom));
 	};
 
 	return (
@@ -74,12 +100,21 @@ export default function AccessibilityMap() {
 				zoom: initialZoom
 			}}
 			mapStyle="https://tiles.openfreemap.org/styles/bright"
+			maxZoom={18}
 			onZoomEnd={onZoomEnd}
 		>
-			<AccessibilityLayer 
-				tileUrl={tileUrl}
-				layerId={layerId}
-			/>
+			<Source id="grids-p5-source" {...gridsP5Source}>
+				<Layer {...gridsP5Style} />
+			</Source>
+			<Source id="grids-p6-source" {...gridsP6Source}>
+				<Layer {...gridsP6Style} />
+			</Source>
+			<Source id="grids-p7-source" {...gridsP7Source}>
+				<Layer {...gridsP7Style} />
+			</Source>	
+			<Source id="edges-p0-source" {...edgesP0Source}>
+				<Layer {...edgesP0Style} />
+			</Source>	
 		</Map>
 	);
 }
